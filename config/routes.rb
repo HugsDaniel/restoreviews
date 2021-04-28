8228645b7c981fed20ce32a17f25556afddd536d
Hugo Daniel <daniel.hugo@protonmail.com>
2021-04-28 09:34:47 +0200

    Restaurant chef & top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  # get "/restaurants/top", to: "restaurants#top", as: :top_restaurants
+  # get "/restaurants/:id/chef", to: "restaurants#chef", as: :restaurant_chef
+
+  resources :restaurants do
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+  end
 end
